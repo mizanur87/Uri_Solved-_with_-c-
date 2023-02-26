7df8a1748ba29c0035b8e717ff224c2a625e86f6
Being_Mizan <71205865+mizanur87@users.noreply.github.com>
2023-02-26 15:59:03 +0600

    Create uri problem 1044-Multiples solved c++.cpp

---
+++ uri problem 1044-Multiples solved c++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cin >> a >> b;
+
+    if (a%2==0 && b%2==0){
+        cout << "Sao Multiplos\n";
+    }
+
+    else {
+        cout << "Nao sao Multiplos\n";
+    }
+
+
+    return 0;
+}
+
+
